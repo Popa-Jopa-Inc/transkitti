d6cedad5eb95ddd8308a893f8ae557bbd1588bc8
Vladislav Knyshov <inova136@gmail.com>
2025-07-19 16:59:44 +0200

    config: setup husky with format/lint checking

--- server/eslint.config.js
+++ server/eslint.config.js
@@ -3,9 +3,12 @@ import globals from "globals";
 import { defineConfig } from "eslint/config";
 import eslintConfigPrettier from "eslint-config-prettier/flat";
 
-
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+  },
   { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.node } },
-  eslintConfigPrettier
+  eslintConfigPrettier,
 ]);
